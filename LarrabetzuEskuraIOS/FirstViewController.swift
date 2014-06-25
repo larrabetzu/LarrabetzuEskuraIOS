07a8ac25b3c93c0070108ece11fd1fa687d11f85
gorka ercilla <ercillagorka@gmail.com>
2014-06-25 13:07:05 +0200

    Initial Commit

---
+++ LarrabetzuEskuraIOS/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  LarrabetzuEskuraIOS
+//
+//  Created by Gorka Ercilla on 25/06/14.
+//  Copyright (c) 2014 gorka. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ LarrabetzuEskuraIOS/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LarrabetzuEskuraIOS
+//
+//  Created by Gorka Ercilla on 25/06/14.
+//  Copyright (c) 2014 gorka. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
