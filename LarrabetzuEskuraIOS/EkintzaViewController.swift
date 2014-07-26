b2dbdd5745ebf0f56fefad28ea589d3b50b436ea
gorka ercilla <ercillagorka@gmail.com>
2014-07-26 16:12:07 +0200

    Ekintzetako kartela hari batetik deskargatzen da.

--- LarrabetzuEskuraIOS/EkintzaViewController.swift
+++ LarrabetzuEskuraIOS/EkintzaViewController.swift
@@ -18,6 +18,7 @@ class EkintzaViewController: UIViewController {
     @IBOutlet var deskribapenaUI: UITextView!
     @IBOutlet var kartelaUI: UIImageView!
     @IBOutlet var linkUI: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     var tituloaString : String = ""
     var hileaString :String = ""
@@ -38,6 +39,8 @@ class EkintzaViewController: UIViewController {
         self.lekuaUI.text = lekuaString
         self.deskribapenaUI.text = deskribapena
         self.linkUI.text = linkString
+        
+        downloadImageBackground()
     }
     
     override func viewWillDisappear(animated: Bool) {
@@ -53,8 +56,31 @@ class EkintzaViewController: UIViewController {
         self.orduaString = ordua
         self.lekuaString = lekua
         self.deskribapena = deskribapena
-        self.kartelaLink = kartela
+        self.kartelaLink = "http://larrabetzu.net/media/"+kartela
         self.linkString = link
+    }
+    
+    func downloadImageBackground(){
+        let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
+        dispatch_async(dispatch_get_global_queue(priority, 0), { ()->() in
+            println("gcd")
+            var urlString: NSString = self.kartelaLink
+            var imgURL: NSURL = NSURL(string: urlString)
+            var request: NSURLRequest = NSURLRequest(URL: imgURL)
+            var urlConnection: NSURLConnection = NSURLConnection(request: request, delegate: self)
+            NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: {(response: NSURLResponse!,data: NSData!,error: NSError!) -> Void in
+                if !error? {
+                    var image: UIImage? = UIImage(data: data)
+                    self.kartelaUI.image = image
+                    self.activityIndicator.stopAnimating()
+                    self.activityIndicator.hidden = true
+                    
+                }else {
+                    println("Error: \(error.localizedDescription)")
+                }
+            })
+       })
+
     }
    
 }
