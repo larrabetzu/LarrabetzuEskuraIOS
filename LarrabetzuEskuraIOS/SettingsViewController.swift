735a96ce1c7286a364f25fb47f0b92bcb01c9da9
gorka ercilla <ercillagorka@gmail.com>
2014-09-15 12:09:12 +0200

    Add class SettingsViewController.

---
+++ LarrabetzuEskuraIOS/SettingsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SettingsViewController.swift
+//  Larrabetzu
+//
+//  Created by Gorka Ercilla on 15/9/14.
+//  Copyright (c) 2014 gorka. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+
+}
